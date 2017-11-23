df9c6509eb8c6e4c02bbe3c67204e89e4c3c10f5
Sirapat <sirapat.naranong@gmail.com>
2017-11-23 23:27:50 +0700

    2nd commit

--- app/src/main/java/com/example/sirapat/eyeagnosis/MainActivity.java
+++ app/src/main/java/com/example/sirapat/eyeagnosis/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -58,6 +59,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent i = new Intent(view.getContext(), CameraActivity2.class);
                 startActivity(i);
+                Log.v("state","going to cameraActivity2");
             }
         });
     }
