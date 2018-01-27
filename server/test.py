2d671da4cc1695c2dcf486a162fc3348c0dd10e2
Sirapat <maito_otplust@hotmail.com>
2018-01-27 14:41:03 +0700

    Update test.py

--- server/test.py
+++ server/test.py
@@ -1,10 +1,10 @@
 import sys
 import cv2
 
+response = {}
+response["imgUrl"] = sys.argv[1]
+response["disease"] = "Pterygium"
+response["possibility"] = 0.9
+response["severity"] = 4
 
-print("hello")
-
-img = sys.argv[1]
-
-print(img)
-
+print(response)
