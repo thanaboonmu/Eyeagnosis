db0a6380307df76ef0b132d6c3ad93821c03a81d
Sirapat <sirapat.naranong@gmail.com>
2018-04-28 21:56:39 +0700

    add  signup/tracking

---
+++ app/src/main/java/com/example/sirapat/eyeagnosis/SignupActivity.java
@@ -0,0 +1,234 @@
+package com.example.sirapat.eyeagnosis;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.gson.JsonObject;
+import com.koushikdutta.async.future.FutureCallback;
+import com.koushikdutta.ion.Ion;
+import com.koushikdutta.ion.Response;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class SignupActivity extends AppCompatActivity {
+
+    private String IP;
+
+    private EditText mUsernameText;
+    private EditText mPasswordText;
+    private EditText mConfirmPasswordText;
+    private EditText mAgeText;
+    private Button mSignupButton;
+    private TextView mSigninLink;
+    private ProgressDialog progressDialog;
+
+    private String gender = "male";
+
+    private int statusCode;
+    private JSONObject signupResponse;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Config config = new Config();
+        IP = config.IP;
+
+        setContentView(R.layout.activity_signup);
+        mUsernameText = (EditText) findViewById(R.id.input_username);
+        mPasswordText = (EditText) findViewById(R.id.input_password);
+        mConfirmPasswordText = (EditText) findViewById(R.id.input_confirm_password);
+        mAgeText = (EditText) findViewById(R.id.input_age);
+        mSignupButton = (Button) findViewById(R.id.btn_signup);
+        mSigninLink = (TextView) findViewById(R.id.link_signin);
+
+        mSignupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signup();
+            }
+        });
+
+        mSigninLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SigninActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.radio_male:
+                if (checked)
+                    gender = "male";
+                    break;
+            case R.id.radio_female:
+                if (checked)
+                    gender = "female";
+                    break;
+        }
+    }
+
+    private void signup() {
+        if(!validate()) {
+            return;
+        }
+        mSignupButton.setEnabled(false);
+
+        progressDialog = new ProgressDialog(SignupActivity.this);
+        progressDialog.setMessage("Sign up, Registering...");
+        progressDialog.setCancelable(false);
+        progressDialog.show();
+
+        callSignupAPI();
+    }
+
+    public void callSignupAPI() {
+
+        String credentials = mUsernameText.getText().toString() + ":" + mPasswordText.getText().toString();
+
+        JsonObject requestObj = new JsonObject();
+        requestObj.addProperty("age", mAgeText.getText().toString());
+        requestObj.addProperty("gender",gender);
+
+        Ion.with(SignupActivity.this)
+                .load("http://" + IP + ":8080/api/users")
+                .setHeader("Authorization", "Basic " + new String(Base64.encode(credentials.getBytes(),Base64.NO_WRAP)))
+                .setHeader("Content-Type", "application/json")
+                .setJsonObjectBody(requestObj)
+                .asJsonObject()
+                .withResponse()
+                .setCallback(new FutureCallback<Response<JsonObject>>() {
+                    @Override
+                    public void onCompleted(Exception e, Response<JsonObject> result) {
+                        if (result != null) {
+                            statusCode = result.getHeaders().code();
+                            try {
+                                signupResponse = new JSONObject(result.getResult().toString());
+                                if(statusCode == 201) {
+                                    String successMessage = signupResponse.getString("message");
+                                    Toast.makeText(SignupActivity.this, successMessage, Toast.LENGTH_LONG).show();
+                                    onSignupSuccess();
+                                } else {
+                                    String errMessage = signupResponse.getString("message");
+                                    Toast.makeText(SignupActivity.this, errMessage, Toast.LENGTH_LONG).show();
+                                    onSignupFailed();
+                                }
+                            } catch (JSONException err) {
+                                Log.e("", "unexpected JSON exception", err);
+                            }
+                        } else {
+                            Log.e("Sign in error: ", e.toString());
+                            Toast.makeText(SignupActivity.this, "Could not connect to the server", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+
+    public void onSignupSuccess() {
+        mSignupButton.setEnabled(true);
+        Thread delayProgressThread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    super.run();
+                    sleep(1500);  // 1.5 seconds of loading
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    progressDialog.dismiss();
+                    Intent i = new Intent(SignupActivity.this, SigninActivity.class);
+                    startActivity(i);
+                }
+            }
+        };
+        if (progressDialog.isShowing()) {
+            delayProgressThread.start();
+        }
+    }
+
+    public void onSignupFailed() {
+        Toast.makeText(getBaseContext(), "Sign up failed", Toast.LENGTH_LONG).show();
+        mSignupButton.setEnabled(true);
+        if(progressDialog.isShowing()) {
+            progressDialog.dismiss();
+        }
+    }
+
+    private boolean validate() {
+
+        boolean valid = true;
+        String username = mUsernameText.getText().toString();
+        String password = mPasswordText.getText().toString();
+        String confirm_password = mConfirmPasswordText.getText().toString();
+        String age = mAgeText.getText().toString();
+
+        if(username.isEmpty() || username.length() < 4 || username.length() > 10) {
+            mUsernameText.setError("Enter a valid username");
+            valid = false;
+        } else {
+            mUsernameText.setError(null);
+        }
+
+        if(password.isEmpty() || password.length() < 4 || password.length() > 10) {
+            mPasswordText.setError("Password should be between 4 and 10 characters");
+            valid = false;
+        } else {
+            mPasswordText.setError(null);
+        }
+
+        if(confirm_password.isEmpty() || !confirm_password.equals(password)) {
+            mConfirmPasswordText.setError("Confirm password should match password");
+            valid = false;
+        } else {
+            mConfirmPasswordText.setError(null);
+        }
+
+        if(age.isEmpty()) {
+            mAgeText.setError("Enter a valid age");
+            valid = false;
+        } else {
+            try {
+                int age_int = Integer.parseInt(age);
+                if(age_int <= 0 || age_int > 100) {
+                    mAgeText.setError("Age should be between 1-100");
+                    valid = false;
+                } else {
+                    mAgeText.setError(null);
+                }
+            } catch (NumberFormatException e) {
+                mAgeText.setError("Please enter a valide age");
+                Log.e("Error", "can not parse age to int");
+                valid = false;
+            }
+        }
+
+        if(gender.isEmpty()) {
+            Log.e("Error", "no gender found");
+            valid = false;
+        } else if(!gender.equals("male") && !gender.equals("female")) {
+            Log.e("Error", "Gender is either male or female");
+            valid = false;
+        }
+
+        return valid;
+    }
+}
---
+++ app/src/main/java/com/example/sirapat/eyeagnosis/TrackActivity.java
@@ -0,0 +1,195 @@
+package com.example.sirapat.eyeagnosis;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.koushikdutta.async.future.FutureCallback;
+import com.koushikdutta.ion.Ion;
+import com.roger.catloadinglibrary.CatLoadingView;
+
+import org.json.JSONArray;
+
+import java.io.File;
+
+/**
+ * Created by sirapat on 28/4/2018 AD.
+ */
+
+public class TrackActivity extends AppCompatActivity {
+
+    private String IP;
+
+    String tempLeftRes;
+    String tempRightRes;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home: {
+                    Intent i = new Intent(getApplicationContext(), CameraActivity.class);
+                    if (tempLeftRes != null) {
+                        i.putExtra("tempLeftRes", tempLeftRes);
+                    }
+                    if (tempRightRes != null) {
+                        i.putExtra("tempRightRes", tempRightRes);
+                    }
+                    startActivity(i);
+                    return true;
+                }
+                case R.id.navigation_camera: {
+                    Intent i = new Intent(getApplicationContext(), CameraActivity.class);
+                    if (tempLeftRes != null) {
+                        i.putExtra("tempLeftRes", tempLeftRes);
+                    }
+                    if (tempRightRes != null) {
+                        i.putExtra("tempRightRes", tempRightRes);
+                    }
+                    startActivity(i);
+                    return true;
+                }
+                case R.id.navigation_result: {
+                    Intent i = new Intent(getApplicationContext(), Result.class);
+                    if (tempLeftRes != null) {
+                        i.putExtra("tempLeftRes", tempLeftRes);
+                    }
+                    if (tempRightRes != null) {
+                        i.putExtra("tempRightRes", tempRightRes);
+                    }
+                    startActivity(i);
+                    return true;
+                }
+                case R.id.navigation_track: {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+    };
+
+    // create an action bar button
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu){
+        SharedPreferences sp = getSharedPreferences("myjwt", Context.MODE_PRIVATE);
+        // if there is token then show sign out button
+        if (!sp.getString("token","").equals("") && !sp.getString("username","").equals("")) {
+            getMenuInflater().inflate(R.menu.signout, menu);
+        }
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+            switch (which) {
+                case DialogInterface.BUTTON_POSITIVE:
+                    //Yes button clicked
+                    SharedPreferences sp = getSharedPreferences("myjwt", Context.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = sp.edit();
+                    editor.putString("token", "");
+                    editor.putString("username", "");
+                    editor.apply();
+                    Intent i = new Intent(TrackActivity.this, SigninActivity.class);
+                    TrackActivity.this.startActivity(i);
+                    break;
+
+                case DialogInterface.BUTTON_NEGATIVE:
+                    //No button clicked
+                    break;
+            }
+        }
+    };
+
+    // handle button activities
+    @Override
+    public boolean onOptionsItemSelected (MenuItem item){
+        int id = item.getItemId();
+
+        if (id == R.id.btn_signout) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(TrackActivity.this);
+            builder.setTitle("Confirmation").setMessage("Sign out?").setPositiveButton("Yes", dialogClickListener)
+                    .setNegativeButton("No", dialogClickListener).show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        Menu menu = navigation.getMenu();
+        MenuItem menuItem = menu.getItem(3);
+        menuItem.setChecked(true);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_track);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        Menu menu = navigation.getMenu();
+        MenuItem menuItem = menu.getItem(3);
+        menuItem.setChecked(true);
+        setTitle("Tracking");
+        Config config = new Config();
+        IP = config.IP;
+
+        Bundle extras = getIntent().getExtras();
+        if(extras != null) {
+            if (extras.getString("tempLeftRes") != null) {
+                tempLeftRes = extras.getString("tempLeftRes");
+            }
+            if (extras.getString("tempRightRes") != null) {
+                tempRightRes = extras.getString("tempRightRes");
+            }
+        } else {
+            Log.e("Temp Res: ", "No temp res yet");
+        }
+
+        SharedPreferences sp = getSharedPreferences("myjwt", Context.MODE_PRIVATE);
+        final String token = sp.getString("token", "");
+        final String username = sp.getString("username", "");
+        final CatLoadingView loading = new CatLoadingView();
+
+        loading.show(getSupportFragmentManager(), "Getting data...");
+        Ion.with(TrackActivity.this)
+                .load("http://" + IP + ":8080/api/users/" + username)
+                .setTimeout(200000)
+                .setHeader("Authorization", "Bearer " + token)
+                .asJsonArray()
+                .setCallback(new FutureCallback<JsonArray>() {
+                    @Override
+                    public void onCompleted(Exception e, JsonArray result) {
+                        loading.dismiss();
+                        if(result != null) {
+                            Log.e("eeee",result.toString());
+                            Toast.makeText(TrackActivity.this, result.toString(), Toast.LENGTH_LONG).show();
+                            for (int i = 0; i < result.size(); i++) {
+                                Log.e(String.valueOf(i),result.get(i).getAsJsonObject().get("possibility").toString());
+                            }
+                        } else {
+                            Toast.makeText(TrackActivity.this, "Server isn't running", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+}
